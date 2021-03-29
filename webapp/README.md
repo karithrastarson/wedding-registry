c9f0cc826cb744786521b15909775b8c83aed8b6
Valgeir Bjornsson <valgeirb@valka.is>
2021-03-29 14:13:47 +0000

    Basic setup

--- webapp/README.md
+++ webapp/README.md
@@ -1,10 +1,10 @@
-# React Web App
+# Vue Web App
 
 ## Running the app
-1. Follow the instructions in parent folder, 
+1. Follow the instructions in parent folder,
 to start the docker container and the web service.
    Instructions found [here](../README.md).
-   
+
 2. Run ``npm start`` this folder.
 
 3. Open ``localhost:3000`` to see the web app.
---
+++ webapp/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <Header class="mb-8"> Gjafalisti </Header>
+    <Item
+      class="flex flex-col w-full mb-4"
+      v-for="item in items"
+      :item="item"
+      :key="item.id"
+    />
+  </div>
+</template>
+
+<script setup>
+  import { ref, onMounted } from "vue";
+  import api from "./api";
+  import Header from "./components/Header.vue";
+  import Item from "./components/Item.vue";
+
+  const items = ref([]);
+  onMounted(async () => {
+    items.value = await api.get("/items");
+  });
+</script>
